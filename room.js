f73161dbbf45258cecbde3499ca1e5981f01f6ce
Vighnaan <77102374+Vighnaan@users.noreply.github.com>
2021-06-08 18:15:35 +0530

    Add files via upload

---
+++ room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCIguWYXBEWJ-UPNwSmc7e5N4cnKxk3W7E",
+    authDomain: "kwitterpro.firebaseapp.com",
+    projectId: "kwitterpro",
+    storageBucket: "kwitterpro.appspot.com",
+    messagingSenderId: "935759179047",
+    appId: "1:935759179047:web:8bb9a07f5c73f9bae1cd02"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
